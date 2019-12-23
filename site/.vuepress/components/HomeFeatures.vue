7cd9a2e02a8b419082609668fb01efd208d0e1cc
Ryan Medina <ryanm@zumasys.com>
2019-12-23 12:00:52 -0800

    Add home page features

---
+++ site/.vuepress/components/HomeFeatures.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="features">
+    <div class="feature">
+      <h2>AccuTerm</h2> 
+      <p>
+        <a href="">Desktop</a><br />
+        <a href="">Web</a><br />
+        <a href="">Mobile</a>
+      </p>
+    </div>
+    <div class="feature">
+      <h2>jBASE</h2> 
+      <p>When you're ready, take a peek here.</p>
+    </div>
+    <div class="feature">
+      <h2>OpenQM</h2> 
+      <p>But wait, there's more</p>
+    </div>
+    <div class="feature">
+      <h2>MV Dashboard</h2> 
+      <p>Come read about the first feature here.</p>
+    </div>
+    <div class="feature">
+      <h2>MV Connect</h2> 
+      <p>When you're ready, take a peek here.</p>
+    </div>
+    <div class="feature">
+    </div>
+  </div> 
+</template>
+
+<script>
+export default {
+    
+}
+</script>
