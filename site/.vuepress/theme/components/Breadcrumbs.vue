e95c98caf0ef34c88465072cc7f2462ee2b90da0
Ryan Medina <ryan@454creative.com>
2020-02-03 16:48:44 -0800

    Add initial breadcrumbs to pages

---
+++ site/.vuepress/theme/components/Breadcrumbs.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="bread-crumbs">
+    <span v-for="crumb in bread" :key="crumb.path">
+      <router-link class="bread-crumb" :to="crumb.path">{{crumb.title}}</router-link> &raquo;
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Breadcrumbs",
+    computed: {
+      bread() {
+        const parts = this.$page.path.split("/");
+
+        // Bascially if there's a trailing slash then get rid of the blank 
+        if (!parts[parts.length - 1].length) { 
+          parts.pop(); 
+        }
+      
+        let link = ''
+        // Loop through the crumbs
+        const crumbs = parts.map(slug => {
+          link += slug
+          const page = this.$site.pages.find((el) => el.path === link || el.path === link + "/");
+          link += '/'
+          // if a page is found
+          if (page) {
+            return { path: page.path, title: page.title || page.frontmatter.breadcrumb }
+          }
+
+        })
+        
+        return crumbs
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" scoped>
+  nav
+    margin-bottom 0
+    background-color #B0BEC5
+    height 64px
+    line-height 64px
+
+    .nav-wrapper
+      position: relative
+      height 100%
+
+  .breadcrumb
+    &::before
+      content "/"
+      font-family inherit
+      font-size inherit
+
+    &:last-child
+      cursor default
+</style>
