8afd51b18d5767edfff1761454282b2607500775
Ryan Medina <ryanm@zumasys.com>
2020-02-12 11:25:45 -0800

    Remove h1/title code from page header component. Temporarily remove git log plugin created computed funtion

--- site/.vuepress/components/PageHeader.vue
+++ site/.vuepress/components/PageHeader.vue
@@ -1,11 +1,7 @@
 <template>
-    <div class="page-header">
-        <h1 :id="anchor">
-            <a :href="'#'+ anchor" class="header-anchor">#</a>
-            {{ getTitle }}
-        </h1>
+    <div class="page-header-details">
         <p>
-            <strong>Updated:</strong> {{ created }}<br />
+            <strong>Updated:</strong> <br />
             <strong>Created:</strong> {{ updated }}<br />
             <strong>Last Updated By:</strong> {{ author }}<br />
             <strong>Read Time:</strong> {{ readingTime }} minute<br />
@@ -16,32 +12,7 @@
 
 <script>
 export default {
-    props: ['title'],
     computed: {
-        getTitle () {
-            let title
-            const path = this.$page.path
-            // If a title is passed as a prop
-            if (this.title) {
-                title = this.title
-            } else {
-                const page = this.$site.pages.find((el) => el.path === path || el.path === path + "/");
-                
-                title = page ? page.title : 'No Title'
-            }
-            
-            return title
-        },
-        anchor () {
-            const parts = this.$page.path.split("/");
-
-            // Bascially if there's a trailing slash then get rid of the blank 
-            if (!parts[parts.length - 1].length) { 
-            parts.pop(); 
-            }
-            
-            return parts[parts.length - 1]
-        },
         // Full object list: //github.com/darrenjennings/vuepress-plugin-reading-time#usage
         readingTime() { 
             let minutes = this.$page.readingTime.minutes
@@ -58,7 +29,7 @@ export default {
         author() { return this.$page.git.author; },
         commits() { return this.$page.git.commits; },
         contributors() { return this.$page.git.contributors; },
-        created() { return this.$page.git.created; },
+        //created() { return this.$page.git.created; },
         updated() { return this.$page.git.updated; }
     }
 }
