15497e1f536bf785d5d12b34a64874a1eedb0e32
Ryan Medina <imagingconstruct@gmail.com>
2020-02-14 16:29:42 -0800

    Add SEO plugin default options

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -44,7 +44,20 @@ module.exports = {
       backgroundColor: '#eb6b3b',
       staticIcon: true
     },
-    "social-share": {}
+    "social-share": {},
+    'seo': { 
+      siteTitle: (_, $site) => $site.title,
+      title: $page => $page.title,
+      description: $page => $page.frontmatter.description,
+      author: (_, $site) => $site.themeConfig.author,
+      tags: $page => $page.frontmatter.tags,
+      twitterCard: _ => 'summary_large_image',
+      type: $page => ['articles', 'posts', 'blog'].some(folder => $page.regularPath.startsWith('/' + folder)) ? 'article' : 'website',
+      url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
+      image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image),
+      publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
+      modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated),
+    }
   },
   themeConfig: {
     logo: "/assets/img/logo-grey.png",
