aa23c9e90d84f2f0ce417b2e869ce8fba5150cc5
Ryan Medina <imagingconstruct@gmail.com>
2020-04-02 14:03:18 -0700

    Clean up config

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -73,17 +73,7 @@ module.exports = {
     "social-share": {},
     // Meta data
     seo: {
-      //siteTitle: (_, $site) => $site.title,
-      //title: $page => $page.title,
-      //description: $page => $page.frontmatter.description,
-      author: ($page, $site) => $page.git.author || $site.themeConfig.author // author: (_, $site) => $site.themeConfig.author,
-      //tags: $page => $page.frontmatter.tags,
-      //twitterCard: _ => 'summary_large_image',
-      //type: $page => ['articles', 'posts', 'blog'].some(folder => $page.regularPath.startsWith('/' + folder)) ? 'article' : 'website',
-      //url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
-      //image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image),
-      //publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
-      //modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated),
+      author: ($page, $site) => $page.git.author || $site.themeConfig.author // author: (_, $site) => $site.themeConfig.author
     }
   },
   themeConfig: {
