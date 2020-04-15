5127f777b885e0cd65904e15aee672837529d5c0
Ryan Medina <imagingconstruct@gmail.com>
2020-04-15 15:10:29 -0700

    Clean up google tag manager code

--- site/.vuepress/enchanceApp.js
+++ site/.vuepress/enchanceApp.js
@@ -10,10 +10,14 @@ export default ({
     //www.analyticsmania.com/post/single-page-web-app-with-google-tag-manager/#developers-help
     if (process.env.NODE_ENV === 'production' && process.env.VUE_APP_GOOGLE_TAG_MANAGER && typeof window !== 'undefined') {
         (function(w,d,s,l,i)    {
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            w[l] = w[l] || [];
+            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+            var f = d.getElementsByTagName(s)[0];
+            var j = d.createElement(s);
+            var dl = l !='dataLayer' ? '&l='+ l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id='+ i + dl;
+            f.parentNode.insertBefore(j,f);
         })(window, document, 'script', 'dataLayer', process.env.VUE_APP_GOOGLE_TAG_MANAGER);
 
         router.afterEach(function (to) {
