a7e178860111f04c2a18d3e168fb4467d36d2568
Shoshana Sokolin <shoshanas@zumasys.com>
2021-10-13 10:49:32 -0700

    Add SSO setup to side menu

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -267,6 +267,7 @@ module.exports = {
               ["/smartsuite/web/advanced-faculty-adoptions-store-side/", "Advanced Faculty Adoptions - Store Side"],
               ["/smartsuite/web/campus-webstore-parameters/", "Campus Webstore Parameters"],
               ["/smartsuite/web/legacy-faculty-adoptions-web-side/", "Legacy Faculty Adoptions - Web"]
+              ["/smartsuite/web/SSO-with-azure-setup/", "SSO with Azure AD - Setup"]
             ]
           },
           ["/smartsuite/release-notes/", "Release Notes"]
